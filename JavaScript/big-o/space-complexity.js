cbfb9c7d9ffb890341c0d58e5f502de7b8e19e2c
mamel001 <leilenah.mamea@disney.com>
2018-12-02 11:53:00 -0800

    Start space complexity examples.

---
+++ JavaScript/big-o/space-complexity.js
@@ -0,0 +1,28 @@
+// --------------------------- //
+//       Space Complexity      //
+// --------------------------- //
+
+'use strict';
+
+/**
+ * Space complexity is a parallel concept to time complexity.
+ *
+ * When considering the space complexity of a function,
+ * you only take into account calls that are simultaneously added to the call stack.
+ */
+
+/**
+ * O(n) - Linear Space
+ *
+ * @param {int} size - size of array
+ */
+function buildArray(size) {
+    const arr = [],
+          counter = 1;
+}
+
+
+buildArray(3);
+
+
+
