66d4d72b7a341fc3771d47b838cfb3adca862f10
mamel001 <leilenah.mamea@disney.com>
2018-12-02 20:50:06 -0800

    Add hash table example.

--- JavaScript/data-structures/hash-table.js
+++ JavaScript/data-structures/hash-table.js
@@ -9,23 +9,44 @@
  *         Overview          //
  * ------------------------- //
  *
+ * Hash table data structures have key-value pairs -- they store data in an associative manner.
+ * Hash tables are also known as dictionaries, associative arrays, hashmaps, maps, etc.
  *
+ * Using a technique called "hashing", you can turn key values into an array of indices.
  *
  * ------------------------- //
  *        When to Use        //
  * ------------------------- //
  *
- *
+ * Use a hash table when you want your structure to use key-value pairs.
+ * Hash tables are very efficient for search and lookup.
  *
  * ------------------------- //
  *     Time Complexities     //
  * ------------------------- //
  *
- *
+ * Search: O(1) avg, O(n) worst
+ * Insertion: O(1) avg, O(n) worst
+ * Deletion: O(1) avg, O(n) worst
  *
  * ------------------------- //
  *       Implementation      //
  * ------------------------- //
  *
- *
  */
+
+ const hashTable = {}; // new Object();
+
+// addiing to hash table v1
+ hashTable['key1'] = 'value1';
+
+// adding to hash table v2
+hashTable.key2 = 'value2';
+
+// hash table lookup
+hashTable['key1'] // 'value1'
+
+// hash table loop
+for (let key in hashTable) {
+    console.log('key: ' + key, 'value: ' + hashTable[key]);
+}
