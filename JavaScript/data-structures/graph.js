638079faab31d35194883c644cfa934220b6eb23
mamel001 <leilenah.mamea@disney.com>
2018-12-02 20:10:29 -0800

    Add graph overview.

--- JavaScript/data-structures/graph.js
+++ JavaScript/data-structures/graph.js
@@ -9,23 +9,34 @@
  *         Overview          //
  * ------------------------- //
  *
+ * A Graph is a non-linear data structure that contains vertices (V) and edges (E).
+ * Vertices are connected by edges.
  *
+ * The edges of a graph can either be directed or undirected.
+ *
+ * Graphs are frequently represented as an Adjacency Matrix or an Adjacency List.
+ * Two nodes are considered adjacent if they are connected by an edge.
+ *
+ * Basic operations: Add Vertex, Add Edge and Display Vertex.
  *
  * ------------------------- //
  *        When to Use        //
  * ------------------------- //
  *
- *
+ * Use a graph data structure when you need to represent a collection of items as interconnected nodes.
  *
  * ------------------------- //
  *     Time Complexities     //
  * ------------------------- //
  *
- *
+ * Adjacency List Search: O(V + E)
+ * Adjacency Matrix Search: O(V²)
  *
  * ------------------------- //
  *       Implementation      //
  * ------------------------- //
  *
- *
+ * Below is an implementation of an adjacency list graph.
  */
+
+// Link: https://www.geeksforgeeks.org/implementation-graph-javascript/
